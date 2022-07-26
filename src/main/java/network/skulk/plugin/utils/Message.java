acaba5684eecb297250c0ebbb953bd0a6fc76356
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-07-26 18:35:44 +0300

    Add colors to the main text in utils.Message

--- src/main/java/network/skulk/plugin/utils/Message.java
+++ src/main/java/network/skulk/plugin/utils/Message.java
@@ -6,10 +6,10 @@ import org.jetbrains.annotations.NotNull;
 
 public abstract class Message {
     public static void sendPlayerOffline(@NotNull Player user) {
-        user.sendRichMessage("<bold><gray>[ <red>!</red> ]</gray></bold> This player is not online.");
+        user.sendRichMessage("<bold><gray>[ <red>!</red> ]</gray></bold> <red>This player is not online.</red>");
     }
 
     public static void sendOnlyPlayer(@NotNull CommandSender user) {
-        user.sendRichMessage("<bold><gray>[ <red>!</red> ]</gray></bold> This command can only be used by players.");
+        user.sendRichMessage("<bold><gray>[ <red>!</red> ]</gray></bold> <red>This command can only be used by players.</red>");
     }
 }
