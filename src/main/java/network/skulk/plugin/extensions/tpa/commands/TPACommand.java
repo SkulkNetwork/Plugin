eb8e0cf0bbbbf802c34795ff99fdd1c9cbc09615
RGBCube <rgbcube@users.noreply.github.com>
2022-07-26 15:11:20 +0300

    Clean TPACommand

--- src/main/java/network/skulk/plugin/extensions/tpa/commands/TPACommand.java
+++ src/main/java/network/skulk/plugin/extensions/tpa/commands/TPACommand.java
@@ -21,13 +21,15 @@ public final class TPACommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (!(sender instanceof Player user)) {
+        if (!(sender instanceof Player player)) {
             Message.sendOnlyPlayer(sender);
             return true;
         }
-        if (args.length != 1) return false;
+        if (args.length != 1) {
+            return false;
+        }
 
-        String userName = user.getName();
+        String playerName = player.getName();
 
         Player target;
         String targetName = args[0];
@@ -35,7 +37,7 @@ public final class TPACommand implements CommandExecutor {
         target = Bukkit.getPlayer(targetName);
 
         if (target == null || !target.isOnline()) {
-            Message.sendPlayerOffline(user);
+            Message.sendPlayerOffline(player);
             return true;
         }
 
@@ -43,36 +45,36 @@ public final class TPACommand implements CommandExecutor {
 
         if (targetIgnores.contains("*")) {
             // Here the target ignores everyone.
-            user.sendRichMessage("<bold><gray>[ <red>!</red> ]</gray> <red>%s</bold> doesn't accept TPA requests from anybody.</red>".formatted(targetName));
+            player.sendRichMessage("<bold><gray>[ <red>!</red> ]</gray> <red>%s</bold> doesn't accept TPA requests from anyone.</red>".formatted(targetName));
             return true;
-        } else if (targetIgnores.contains(userName)) {
-            // Here the target ignores the user.
-            user.sendRichMessage("<bold><gray>[ <red>!</red> ]</gray> <red>%s</bold> doesn't accept TPA requests from you.</red>".formatted(targetName));
+        } else if (targetIgnores.contains(playerName)) {
+            // Here the target ignores the player.
+            player.sendRichMessage("<bold><gray>[ <red>!</red> ]</gray> <red>%s</bold> doesn't accept TPA requests from you.</red>".formatted(targetName));
             return true;
         }
 
-        HashSet<String> targetRequests = extension.tpaRequests.computeIfAbsent(targetName, k -> new HashSet<>());
+        HashSet<String> targetIncomingRequests = extension.tpaRequests.computeIfAbsent(targetName, k -> new HashSet<>());
 
-        // The user already has a TPA request sent to the target.
-        if (targetRequests.contains(userName)) {
-            user.sendRichMessage("<bold><gray>[ <red>!</red> ]</gray></bold> <red>You already have a pending TPA request to <bold>%s</bold>.</red>".formatted(targetName));
+        // The player already has a TPA request sent to the target.
+        if (targetIncomingRequests.contains(playerName)) {
+            player.sendRichMessage("<bold><gray>[ <red>!</red> ]</gray></bold> <red>You already have a pending TPA request to <bold>%s</bold>.</red>".formatted(targetName));
             return true;
         }
 
         // Adds the request.
-        targetRequests.add(userName);
+        targetIncomingRequests.add(playerName);
 
         // Removing the request 60 seconds later.
         extension.runTaskLater(60, () -> {
-            if (targetRequests.contains(userName)) {
-                targetRequests.remove(userName);
-                user.sendRichMessage("<bold><gray>[ <red>!</red> ]</gray></bold> <red>Your TPA request to <bold>%s</bold> has expired.</red>".formatted(targetName));
-                target.sendRichMessage("<bold><gray>[ <red>!</red> ]</gray></bold> <red>The TPA request <bold>%s</bold> has sent to you has expired.</red>".formatted(userName));
+            if (targetIncomingRequests.contains(playerName)) {
+                targetIncomingRequests.remove(playerName);
+                player.sendRichMessage("<bold><gray>[ <color:#ffae1a>!</color> ]</gray></bold> <color:#ffae1a>Your TPA request to <bold>%s</bold> has expired.</color>".formatted(targetName));
+                target.sendRichMessage("<bold><gray>[ <color:#ffae1a>!</color> ]</gray></bold> <color:#ffae1a>The TPA request <bold>%s</bold> has sent to you has expired.</color>".formatted(playerName));
             }
         });
 
-        user.sendRichMessage("<bold><gray>[ <green>✓</green> ]</gray></bold> <green>Sent a TPA request to <bold>%s</bold>.</green>".formatted(targetName));
-        target.sendRichMessage("<bold><gray>[ <blue>?</blue> ]</gray> %s</bold> has sent a TPA request to you. Do you accept? <bold><green><click:run_command:/tpa-accept %s>[✓]</click> </green><red><click:run_command:/tpa-reject %s>[✗]</click></red></bold>".formatted(userName, userName, userName));
+        player.sendRichMessage("<bold><gray>[ <green>✓</green> ]</gray></bold> <green>Sent a TPA request to <bold>%s</bold>.</green>".formatted(targetName));
+        target.sendRichMessage("<bold><gray>[ <blue>?</blue> ]</gray> %s</bold> has sent a TPA request to you. Do you accept? <bold><green><click:run_command:/tpa-accept %s>[✓]</click></green> <red><click:run_command:/tpa-reject %s>[✗]</click></red></bold>".formatted(playerName, playerName, playerName));
 
         return true;
     }
