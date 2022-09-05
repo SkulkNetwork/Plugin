c8a48ab695a8c41d5229b18ca98596901f8eaa44
RGBCube <rgbcube@users.noreply.github.com>
2022-09-05 13:06:52 +0300

    Save data to a file

--- src/main/java/network/skulk/pluginrewrite/extensions/tpa/TPAExtension.java
+++ src/main/java/network/skulk/pluginrewrite/extensions/tpa/TPAExtension.java
@@ -2,13 +2,24 @@ package network.skulk.pluginrewrite.extensions.tpa;
 
 import com.google.common.collect.HashMultimap;
 import network.skulk.wrapper.BaseExtension;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.PrintWriter;
 
 public final class TPAExtension extends BaseExtension {
     public final HashMultimap<String, String> tpaRequests = HashMultimap.create();
-    public final HashMultimap<String, String> tpaIgnores = HashMultimap.create();
+    public final File tpaIgnoresFile = new File(this.getDataFolder(), "tpaIgnores.yml");
+    public HashMultimap<String, String> tpaIgnores;
 
     @Override
     public void onEnableHook() throws Exception {
+        this.tpaIgnores = new Yaml().load(new FileInputStream(this.tpaIgnoresFile));
+    }
 
+    @Override
+    public void onDisableHook() throws Exception {
+        new Yaml().dump(tpaIgnores, new PrintWriter(this.tpaIgnoresFile));
     }
 }
