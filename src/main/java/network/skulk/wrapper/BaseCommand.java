487dfae36a44161df5ed4ced9dcb7c4ac173438e
RGBCube <rgbcube@users.noreply.github.com>
2022-09-06 11:04:53 +0300

    add TabCompleter

--- src/main/java/network/skulk/wrapper/BaseCommand.java
+++ src/main/java/network/skulk/wrapper/BaseCommand.java
@@ -3,13 +3,17 @@ package network.skulk.wrapper;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitTask;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 import static network.skulk.utils.MiniMessageFormat.sendMessage;
 import static org.jetbrains.annotations.ApiStatus.OverrideOnly;
 
-public abstract class BaseCommand<E extends BaseExtension> implements CommandExecutor {
+public abstract class BaseCommand<E extends BaseExtension> implements CommandExecutor, TabCompleter {
     protected int minArgs;
     protected String[] aliases;
     protected boolean playerOnly;
@@ -33,6 +37,18 @@ public abstract class BaseCommand<E extends BaseExtension> implements CommandExe
     protected void init() {
     }
 
+    @Nullable
+    @OverrideOnly
+    protected List<String> tabComplete(final CommandSender player, final String[] args) {
+        return null;
+    }
+
+    @Nullable
+    @OverrideOnly
+    protected List<String> tabComplete(final Player player, final String[] args) {
+        return null;
+    }
+
     @OverrideOnly
     protected boolean execute(final CommandSender sender, final String[] args) {
         return false;
@@ -51,6 +67,16 @@ public abstract class BaseCommand<E extends BaseExtension> implements CommandExe
     @OverrideOnly
     protected boolean execute(final Player player) {
         return false;
+    }
+
+    @Nullable
+    @Override
+    public List<String> onTabComplete(final CommandSender sender, final Command command, final String label, final String[] args) {
+        if (this.playerOnly) {
+            return tabComplete((Player) sender, args);
+        } else {
+            return tabComplete(sender, args);
+        }
     }
 
     @Override
--- src/main/java/network/skulk/wrapper/BasePlugin.java
+++ src/main/java/network/skulk/wrapper/BasePlugin.java
@@ -32,6 +32,7 @@ public abstract class BasePlugin extends JavaPlugin {
             }
 
             cmd.setExecutor(command);
+            cmd.setTabCompleter(command);
         }
     }
 
