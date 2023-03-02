ed5b4e14584b16cb9f2b39463c102314c887a135
RGBCube <RGBCube@users.noreply.github.com>
2023-03-02 22:57:23 +0300

    SNPlugin: Remove explicit generic

--- src/main/kotlin/tk/skulk/plugin/core/SNPlugin.kt
+++ src/main/kotlin/tk/skulk/plugin/core/SNPlugin.kt
@@ -4,7 +4,6 @@ import com.google.gson.Gson
 import net.kyori.adventure.text.minimessage.MiniMessage
 import okhttp3.OkHttpClient
 import tk.skulk.plugin.core.extensions.entityOverride.EntityOverrideExtension
-import tk.skulk.plugin.framework.Extension
 import tk.skulk.plugin.framework.Plugin
 import tk.skulk.plugin.pdts.BooleanPersistentDataType
 
@@ -15,7 +14,7 @@ val miniMessage = MiniMessage.miniMessage()
 
 
 class SNPlugin : Plugin() {
-    override val extensions = listOf<() -> Extension<SNPlugin>>(
+    override val extensions = listOf(
         { EntityOverrideExtension(this) },
     )
 }
