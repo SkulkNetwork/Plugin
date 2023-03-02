b837a856544942016a929c96a1c34a3492bf7872
RGBCube <RGBCube@users.noreply.github.com>
2023-03-02 22:02:28 +0300

    Constants: Move to SNPlugin

--- src/main/kotlin/tk/skulk/plugin/core/SNPlugin.kt
+++ src/main/kotlin/tk/skulk/plugin/core/SNPlugin.kt
@@ -1,7 +1,14 @@
 package tk.skulk.plugin.core
 
+import com.google.gson.Gson
+import net.kyori.adventure.text.minimessage.MiniMessage
+import okhttp3.OkHttpClient
 import tk.skulk.plugin.framework.Extension
 import tk.skulk.plugin.framework.Plugin
+
+val miniMessage = MiniMessage.miniMessage()
+val gson = Gson()
+val http = OkHttpClient()
 
 class SNPlugin : Plugin() {
     override val extensions = listOf<() -> Extension<SNPlugin>>(
