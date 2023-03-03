3a1ffe66df9bc5eb1bb50d675de510e6cdbbbd87
RGBCube <RGBCube@users.noreply.github.com>
2023-03-03 22:05:21 +0300

    Util: Merge component and message util

--- src/main/kotlin/tk/skulk/plugin/util/ComponentUtil.kt
+++ src/main/kotlin/tk/skulk/plugin/util/ComponentUtil.kt
@@ -1,6 +1,50 @@
 package tk.skulk.plugin.util
 
+import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
+import tk.skulk.plugin.core.miniMessage
 import tk.skulk.plugin.core.ptSerializer
 
+private const val template = "<b><gray>[ <color:%s>%s</color> ]</gray></b> <color:%s>%s</color>"
+
 fun Component.asString() = ptSerializer.serialize(this)
+
+fun format(text: String, vararg placeholders: String) = miniMessage.deserialize(
+    text, *placeholders.mapIndexed { i, s ->
+    Placeholder.unparsed(i.toString(), s)
+}.toTypedArray()
+)
+
+fun formatWithComponents(text: String, vararg placeholders: Component) = miniMessage.deserialize(
+    text, *placeholders.mapIndexed { i, c ->
+    Placeholder.component(i.toString(), c)
+}.toTypedArray()
+)
+
+fun makeMessage(
+    color: String,
+    symbol: Char,
+    text: String,
+    vararg placeholders: String,
+) = format(
+    template.format(color, symbol, color, text), *placeholders
+)
+
+fun makeMessageWithComponents(
+    color: String,
+    symbol: Char,
+    text: String,
+    vararg placeholders: Component,
+) = formatWithComponents(
+    template.format(color, symbol, color, text), *placeholders
+)
+
+fun Audience.sendMessage(
+    color: String,
+    symbol: Char,
+    message: String,
+    vararg placeholders: String,
+) {
+    sendMessage(makeMessage(color, symbol, message, *placeholders))
+}
