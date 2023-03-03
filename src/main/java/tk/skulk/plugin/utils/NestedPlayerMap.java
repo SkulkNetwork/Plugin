cd9eb62a65d3f578f0eb9166821e092c04fd9e7e
RGBCube <RGBCube@users.noreply.github.com>
2023-03-03 23:34:01 +0300

    OldCore: Delete redundant files

--- src/main/java/tk/skulk/plugin/utils/NestedPlayerMap.java
+++
@@ -1,9 +0,0 @@
-package tk.skulk.plugin.utils;
-
-import org.jetbrains.annotations.NotNull;
-
-public final class<NestedPlayerMap<V> extends HashMap<Player, HashMap<Player, V>>{
-public @NotNull HashMap<@NotNull Player, V> get(final @NotNull Player key){
-    return this.computeIfAbsent(key,k->new HashMap<>());
-    }
-    }
